bfa9c31386ef0570c208930121b265d68772014b
Andres Hernandez <76182058+ProgrammingDinosaur@users.noreply.github.com>
2022-02-21 17:05:23 -0600

    Add files via upload

--- Proyecto/Programa.cpp
+++ Proyecto/Programa.cpp
@@ -7,24 +7,31 @@
 using namespace std;
 
 class CitaGym {
-private:
-    string nombre_completo;
-    int duracion_horas = 1;
-    int duracion_minutos = 30;
-    vector<int> ids_credencial ={8352,43523,3325245};
+    struct Hora
+    {
+        int horas = 0;
+        int minutos = 0;
+    };
+    private:
+        string nombre_completo;
+        vector<int> ids_credencial ={8352,43523,3325245};
         int id_usuario = 0;
+        struct Hora duracion;
 
 
-   public: 
+    public: 
         int hora_entrada = 12;
         int minutos_entrada= 0;
-        int hora_salida = 0;
-        int minutos_salida = 0;
+        int dur_hor = 0;
+        int dur_min = 0;
         string gym_name = "GYM GYM";
    
-        CitaGym(string nombre, int credencial) {
+        CitaGym(string nombre, int credencial,int hora_ent,int min_ent,int horas_dur,int minu_dur) {
             cout << "Bienvenido a Gimnasio " << gym_name << endl;
             cout << "Hola " <<nombre << " comenzando tu registro..." << endl;
+            hora_entrada = hora_ent;
+            dur_hor = horas_dur;
+            dur_min = minu_dur;
             nombre_completo = nombre;
             id_usuario = credencial;
             bool valido = false;
@@ -45,7 +52,36 @@ private:
             }
 
         }
+        void cambiar_ids_validos(vector<int> ids_new) {
+            ids_credencial = ids_new;
+        }
 
+        vector<int> get_valid_ids() {
+            return ids_credencial;
+        }
+
+        struct Hora calcular_duracion(int hora_ent,int min_ent,int hora_dura,int min_dura) {
+            struct Hora hora_sal= { 0,0 };
+            int min_dif = min_ent + min_dura;
+            if (min_dif >= 60) {
+                hora_sal.horas = hora_ent + hora_dura + 1;
+                hora_sal.minutos = min_dif - 60;
+
+            }
+            else {
+
+                hora_sal.horas = hora_ent + hora_dura;
+                hora_sal.minutos = min_dif;
+
+            }
+            return hora_sal;
+        }
+
+        void registrar_salida() {
+            struct Hora hora_salida = calcular_duracion(hora_entrada, minutos_entrada, dur_hor, dur_min);
+            cout <<nombre_completo << " Empezando Registro de salida... Hora de Salida " << to_string(hora_salida.horas) << ":" << to_string(hora_salida.minutos) <<endl;
+            cout << "Gracias por tu visita " << nombre_completo;
+        }
 
 
 
@@ -56,6 +92,7 @@ public:
     int id_credencial;
     string nombre;
 
+
     Persona(string _nombre, int credencial) {
         nombre = _nombre;
         id_credencial = credencial;
@@ -65,11 +102,16 @@ public:
 
 int main()
 {
+    //Persona Parametros
+    //1.Nombre, 2. ID
     Persona p1("Roberto",899989);
     Persona p2("Gerardo",8352);
 
-    CitaGym cita1(p1.nombre,p1.id_credencial);
-    CitaGym cita2(p2.nombre, p2.id_credencial);
-
+    //Cita GYM Parametros:
+    //1.Nombre Persona 2. ID 3. Hora Entrada (Horas) 4.Hora entrada (Minutos) 5.Duracion cita (Horas) 6. Duracion cita (Minutos)
+    //Cita GYM Métodos
+    CitaGym cita1(p1.nombre,p1.id_credencial,12,30,1,30); //Entrada Roberto
+    CitaGym cita2(p2.nombre, p2.id_credencial,14,30,1,30); //Entrada Gerardo
+    cita2.registrar_salida(); //Salida Gerardo
 
 }
